be16ed6e17b6288928d5afeccc551a46b778c640
yoonsuk914 <yoonsuk914@naver.com>
2025-07-24 13:50:33 +0900

    컴포넌트 공유 완료

---
+++ vue-comp/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import FirstChild from './components/FirstChild.vue';
+export default {
+  components: {
+    FirstChild,
+  },
+  data() {
+    return {
+      parentNum: 5,
+    };
+  },
+  computed: {
+    numOddEven() {
+      return this.parentNum % 2 === 0 ? '짝수' : '홀수'
+    },
+  },
+  methods: {
+    getParentNum() {
+      return this.parentNum;
+    }
+  },
+}
+</script>
+<template>
+  <FirstChild />
+</template>
+<style scoped>
+</style>
---
+++ vue-comp/src/components/UserProfile.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  methods: {
+    parentEventCall() {
+      this.$emit('print-hello', '철수', 30);
+    },
+  },
+}
+</script>
+<template>
+  <button @click="$emit('print-hello', '철수', 30)">클릭</button>
+</template>
